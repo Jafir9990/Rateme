a44e59acbc190689e73d2c288ac72db5ef7832ce
Jafir9990 <122177896+Jafir9990@users.noreply.github.com>
2023-05-11 18:24:59 +0500

    day-4-setup-closed

--- client/src/App.js
+++ client/src/App.js
@@ -7,9 +7,15 @@ import { showSuccess } from './store/actions/alertActions';
 import { hideProgressBar, showProgressBar } from './store/actions/progressBarAction';
 import ProgressBar from './components/library/ProgressBar';
 import AppPublic from './AppPublic';
+import { useEffect } from 'react';
+import { loadAuth, loadToken } from './store/actions/authActions';
 
 function App() {
 const dispatch = useDispatch();
+useEffect(() => {
+  dispatch(loadToken())
+  dispatch(loadAuth() )
+},[])
 return <AppPublic />
   return (
     <Box height="100%" className="App">
--- client/src/components/auth/SignIn.js
+++ client/src/components/auth/SignIn.js
@@ -1,16 +1,28 @@
-import { Box, Button } from '@mui/material'
+import { Box, Button, CircularProgress } from '@mui/material'
 import React from 'react'
 import { Field, Form } from 'react-final-form'
 import TextInput from '../library/form/TextInput';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { showError } from '../../store/actions/alertActions';
+import { useDispatch } from 'react-redux';
+import { signin } from '../../store/actions/authActions';
 
 export default function Signin() {
+  const dispatch = useDispatch()
   return (
     <Box borderRadius="5px" boxShadow="0px 0px 17px 5px #dbdada" p={3} bgcolor='#fff' textAlign="center" minWidth="350px">
       <h3>Rate Me</h3>
       <Form
       onSubmit={(data) =>{
-        console.log('sumbitting',data);
+        return axios.post('/users/signin', data).then(({data}) => {
+          dispatch(signin(data.user, data.token))
+          localStorage.setItem('token',data.token)
+        }).catch(err => {
+          let message = err && err.response && err.response.data ? err.response.data.error : err.message
+          dispatch( showError(message));
+          
+        })
       }}
       validate={(data) =>{
         const errors = {};
@@ -27,11 +39,12 @@ export default function Signin() {
       >
         {
           (props) =>{
+            const {invalid, submitting} = props;
             return(
               <form onSubmit={props.handleSubmit}>
-                <Field name='email' type='email' component={TextInput} placeholder='Enter Email Adress...' />
+                <Field name='email' type='email' component={TextInput} placeholder='Enter Email Adress...' autoFocus/>
                 <Field name='password' type='password' component={TextInput} placeholder='Enter Your Password...' />
-                <Button type='submit' variant='outlined'>Sign In</Button>
+                <Button type='submit' variant='outlined' disabled={invalid || submitting}>Sign In {submitting && <CircularProgress style={{marginLeft:"10px"}} size={15} /> } </Button>
                 <Box mt={2}>
                 <Link style={{textDecoration:"none"}} to="/admin/forgot-password">Forgot Password</Link>
                 </Box>
