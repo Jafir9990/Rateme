78d7bbef3648a9e05d7ae47918a1c52ab699d1b9
Jafir9990 <122177896+Jafir9990@users.noreply.github.com>
2023-05-13 17:53:41 +0500

    client-day-5

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 import Alert from './components/library/Alert';
-import { useDispatch } from 'react-redux';
+import { connect, useDispatch } from 'react-redux';
 import { Box, Button } from '@mui/material';
 import { showSuccess } from './store/actions/alertActions';
 import { hideProgressBar, showProgressBar } from './store/actions/progressBarAction';
 import ProgressBar from './components/library/ProgressBar';
 import AppPublic from './AppPublic';
 import { useEffect } from 'react';
-import { loadAuth, loadToken } from './store/actions/authActions';
+import { loadAuth, signout } from './store/actions/authActions';
+import AppPreLoader from './components/library/AppPreLoader';
 
-function App() {
-const dispatch = useDispatch();
-useEffect(() => {
-  dispatch(loadToken())
-  dispatch(loadAuth() )
-},[])
-return <AppPublic />
+function App({ user, isAuthLoaded, loadAuth, signout }) {
+  useEffect(() => {
+    loadAuth()
+  }, [])
+  if (!isAuthLoaded) {
+    return <AppPreLoader message="loading App....." />
+  }
+  if (!user)
+    return <AppPublic />
   return (
     <Box height="100%" className="App">
-     <ProgressBar />
+      you are SIGNED In
+      <Button onClick={signout}>Logout</Button>
     </Box>
-   
+
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    user: state.auth.user,
+    isAuthLoaded: state.auth.isLoaded,
+  }
+}
+
+export default connect(mapStateToProps, { loadAuth, signout })(App);
